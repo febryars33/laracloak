009847af20a4b310189ce02ce0049724a4d91cc6
Febriansyah Riki Setiadi <febriansyahrikisetiadi@gmail.com>
2026-09-03 14:55:44 +0700

    update: fix error run-test

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -1,6 +1,15 @@
 <?php
 
 it('registers the login route', function () {
+    // Mock the HTTP request to prevent actual external calls
+    \Illuminate\Support\Facades\Http::fake([
+        'http://localhost:8000/.well-known/openid-configuration' => \Illuminate\Support\Facades\Http::response([
+            'authorization_endpoint' => 'https://provider.example.com/authorize',
+            'token_endpoint' => 'https://provider.example.com/token',
+            'userinfo_endpoint' => 'https://provider.example.com/userinfo',
+        ]),
+    ]);
+
     $this->get('/auth/login')
         ->assertRedirect();
 });
